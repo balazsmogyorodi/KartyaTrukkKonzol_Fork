d29141c585cbccb3f21492e9edf05a4c3f629304
Mogyoródi Balázs <mogyorodi.balazs@dk.akkszalezi.hu>
2023-09-18 15:22:28 +0200

    Opp alakitás

---
+++ src/kartyatrukkkonzolon/Lap.java
@@ -0,0 +1,27 @@
+
+package kartyatrukkkonzolon;
+
+
+public class Lap {
+    private String leiras;
+
+    public Lap(String leiras) {
+        this.leiras = leiras;
+    }
+
+    public String getLeiras() {
+        return leiras;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
+
